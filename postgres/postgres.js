b985ee9f5768c3abe506371f3daa98893e77be19
Vamsi Vinay Kumar Bosara <listenwhatilisten@gmail.com>
2024-11-18 22:28:04 +0530

    modified postgress to have env file access

--- postgres/postgres.js
+++ postgres/postgres.js
@@ -8,10 +8,16 @@ import {
   createPosePathsModel,
 } from '../model/userSchema.js';
 
-const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
-  host: 'localhost', // TODO
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Database user
+  process.env.DB_PASSWORD, // Database password
+  {
+    host: process.env.DB_HOST, // Database host (e.g., RDS endpoint)
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432, // Default to port 5432 if not specified
+  }
+);
 
 let UserModel,
   PosesModel,
